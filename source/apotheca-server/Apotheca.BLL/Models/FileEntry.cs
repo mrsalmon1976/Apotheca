aa590ae8ef1d4ce25b8ac72b96f70da135a921be
Matt Salmon <matt@iua.co.za>
2019-05-12 20:10:42 +0200

    Style changes

---
+++ source/apotheca-server/Apotheca.BLL/Models/FileEntry.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Apotheca.BLL.Models
+{
+    public class FileEntry
+    {
+        public FileEntry()
+        {
+            this.Id = Guid.NewGuid();
+        }
+
+        /// <summary>
+        /// Unique reference for this file entry.
+        /// </summary>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// Unique reference for a single file - common across all versions of a single file entry.
+        /// </summary>
+        public Guid FileId { get; set; }
+
+        public string Name { get; set; }
+
+        public bool IsFolder { get; set; }
+
+        public long Length { get; set; }
+
+        public string ContentType { get; set; }
+
+        public Guid? Parent { get; set; }
+
+        public string Location { get; set; }
+
+        public DateTime Created { get; set; }
+
+        public DateTime Updated { get; set; }
+
+        public int Version { get; set; }
+    }
+}
